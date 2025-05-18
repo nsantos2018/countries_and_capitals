d490ce885cf66033762928cfcda264f6a0840636
Natanael Santos <natanael.santos.gomes@gmail.com>
2025-05-18 19:17:10 -0300

    002 - Create Components and Views

---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+<x-main-layout pageTitle="Countries & Capitals Quiz">
+
+    <h1 class="text-center my-5">Home Page</h1>
+
+</x-main-layout>
--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,12 @@
 
 use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
-
+/*
 Route::get('/', function () {
     echo "hello!";
 });
+*/
+
+Route::view('/', 'home');
 
 Route::get('/show_data', [MainController::class, 'showData']);
