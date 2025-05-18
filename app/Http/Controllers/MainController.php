acc8ce14f15472ca7efb3ac67b01ab4c7489b3bb
Natanael Santos <natanael.santos.gomes@gmail.com>
2025-05-18 19:38:53 -0300

    003 - Create functions in MainController and Routes for prepare quiz of the game

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
@@ -14,8 +15,42 @@ class MainController extends Controller
         $this->app_data = require(app_path('app_data.php'));
     }
 
+    public function startGame(): View
+    {
+        return view('home');
+    }
+
+    public function prepareGame(Request $request)
+    {
+        //validate request
+        $request->validate(
+            ['total_questions' => 'required|integer|min:3|max:30'],
+            [
+                'total_questions.required' => 'O número de questões é obrigatório',
+                'total_questions.integer' => 'O número de questões precisa ser um valor inteiro',
+                'total_questions.min' => 'No mínimo :min questões',
+                'total_questions.max' => 'No máximo :max questões',
+            ]
+        );
+        
+        // get total questions
+        $total_questions = intval($request->input('total_questions'));
+
+        // prepare all the quiz structure
+        $quiz = $this->prepareQuiz($total_questions);
+
+        dd($quiz);
+    }
+
+    private function prepareQuiz($total_questions)
+    {
+        
+    }
+
+    /*
     public function showData()
     {
         return response()->json($this->app_data);
     }
+    */
 }
--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,18 @@
 
 use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
+
+// start Game
+Route::get('/', [MainController::class, 'startGame'])->name('start_game');
+Route::post('/', [MainController::class, 'prepareGame'])->name('prepare_game');
+
+
+
 /*
 Route::get('/', function () {
     echo "hello!";
 });
-*/
 
 Route::view('/', 'home');
-
 Route::get('/show_data', [MainController::class, 'showData']);
+*/
